6f5409d7527bb02b4727a3df8c4a4e6e4bbb2e0e
trinhphu9872 <trinhphu9872@gmail.com>
2021-06-20 04:40:34 +0700

    code

--- Frontend/ChiTietSanPham.php
+++ Frontend/ChiTietSanPham.php
@@ -6,47 +6,62 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sản Phẩm</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css">
-    <link rel="stylesheet" href="style/mainSanPham.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="style/chitiet.css">
     <style>
         .sidebar{
-            height: 1000px;
+            height: 1010px;
+        }
+        a{
+            color:#fff;
         }
     </style>
     
 </head>
-<body>
+<body style="background-color:rgba(199, 194, 84, 0.35)" >
     <?php
-        include("sidebar.php");    
+        include("navbar.php");
     ?>
-
-   
-    <h1 class="text-center mb-5 " style="font-size:100px">Chi tiết sản phẩm</h1>
-    <div class="main " height='85%'>
-    <?php
+    <div class="container-fluid">
+  
+    
+    <div class="main my-4">
+        <div class="row">
+        <?php
             include("connectdb.php");
             $id = $_GET["id"];
             $lenh = "SELECT * FROM products where prd_id = $id";
             $res = mysqli_query($conn,$lenh);
             while( $row = mysqli_fetch_row($res)){
+                if ($row[5] > 0) {
+                    $hang = "Còn Hàng";
+                }else{
+                    $hang = "Hết Hàng";
+                }
                 echo"
                     <form method='post' class=''  method='post' >
-                           <div class='row'>
-                                <div class='col-5 ml-5'><img class='' src='pic/SanPham/$row[2]' width='550px' alt='' class='img-sanpham'></div>
-                                <div class='col-6'>
+                           <div class='row mx-auto'>
+                                <div class='text-center col-5 m-5'>
+                                    <img class='my-3'  src='pic/SanPham/$row[3]' width='400px' alt='' class='img-sanpham'>
+                                    <p >***Lưu Ý: Hình ảnh chỉ mang tính chất minh hoạ. Sản phẩm là đá tự nhiên nên vân đá có thể thay đổi.</p>
+                                </div>
+                                <div class='col-5 mt-5'>
                                     <div class='text-sanpham'>
-                                    <h1 class='my-4' >Sản Phẩm $row[1]</h1>
-                                    <h3 class='my-4' >Mã sản phẩm: SP001048-TCCB-2</h3>
-                                    <h4 class='my-4'>Tồn  kho $row[4]</h4>
-                                    <h4 class='my-4'>NỘI DUNG SẢN PHẨM:</h4>
-                                    <h4 class='my-4'>MẠNG PHÙ HỢP: <p class='element'>HỎA - THỔ</p> </h4>
-                                    <hr>
-                                    <h2>Giá Tiền : $row[3]</h2>
-                                     
-                                    <div>
-                                        <label for='name'>Số Lượng</label>
-                                        <input type='number' name='french-hens' id='french-hens' value='1' class='input'>
+                                    <h4 class='mb-4 mt-3' > $row[1]  <span style='color:red'> ($hang)</span> </h4>
+                                    <div class='mb-4'>
+                                         <p class='m-0' >Mã sản phẩm: $row[2]</p>
+                                         <p class='m-0' >Trạng Thái: New </ơ>
                                     </div>
-                                    <input  type='submit' class='btn btn-danger  px-5 my-4' value='Thêm vào giỏ'>
+                                    <p class='m-0'><h5>Nôi Dung sản phẩm :</h5> $row[6]</p>
+                                    <h5 class=''>Mạng phù hợp: $row[8] </h5>
+                                    <hr>
+                                    <h5 class='text-center my-3 text-danger'> $row[4] VND</h5>
+                                    <div class='text-center'>
+                                        <label for='name'>Số Lượng</label>
+                                        <input type='number' name='french-hens' id='french-hens' value='1' class='input mx-3'>
+                                        <input  type='submit' class='btn btn-danger rounded-pill  mx-3  px-4 ' value='Thêm vào giỏ'>
+                                    </div>
+                                   
 
 
                                
@@ -58,17 +73,55 @@
             };
         
         ?>
+        </div>
+
+    </div>
+        <div class="row ">
+            <div class="col-10 text-center mx-auto">
+            <div class="tab">
+            <ul class="nav nav-tabs" id="myTab" role="tablist">
+                <li class="nav-item" role="presentation">
+                    <a style="color:black" class="nav-link mx-2 active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">Thông tin vận chuyển</a>
+                </li>
+                <li class="nav-item" role="presentation">
+                    <a style="color:black" class="nav-link mx-2" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">Chính sách hoàn chả</a>
+                </li>
+            </ul>
+        <div class="tab-content" id="myTabContent">
+            <div class="tab-pane border-0 p-4 fade show active text-left" id="home" role="tabpanel" aria-labelledby="home-tab">
+                
+            <h4>THÔNG TIN VẬN CHUYỂN </h4>
+            <p> Giao hàng</p>
+            <p>– Nội thành: Giao từ 1 – 3 ngày; Miễn phí giao hàng trong bán kính 10km</p>
+            <p>– Tỉnh khác: Giao từ 5 – 7 ngày; 30.000 VNĐ / đơn</p>
+            <p>** Lưu ý: Thời gian nhận hàng có thể thay đổi sớm hoặc muộn hơn tùy theo địa chỉ cụ thể của khách hàng.</p>
+            <p>*** Trong trường hợp sản phầm tạm hết hàng, nhân viên CSKH sẽ liên hệ trực tiếp với quý khách để thông báo về thời gian giao hàng.
+Nếu khách hàng có yêu cầu về Giấy Kiểm Định Đá, đơn hàng sễ cộng thêm 20 ngày để hoàn thành thủ tục.</p>
+
+            </div>
+            <div class="tab-pane border-0 p-4 fade text-left" id="profile" role="tabpanel" aria-labelledby="profile-tab">
+                <h4>Chính sách hoàn trả</h4>
+            <p> – Chúng tôi chấp nhận đổi / trả sản phẩm ngay lúc khách kiểm tra và xác nhận hàng hóa. Chúng tôi cam kết sẽ hỗ trợ và áp dụng chính sách bảo hành tốt nhất tới Quý khách, đảm bảo mọi quyền lợi Quý khách được đầy đủ.</p>
+            <p>   – Những trình trạng bể, vỡ do quá trình quý khách sử dụng chúng tôi xin từ chối đổi hàng.</p>
+            <p>   – Tùy vào tình hình thực tế của sản phẩm , chúng tôi sẽ cân nhắc hỗ trợ đổi / trả nếu sản phẩm lỗi hoặc các vấn đề liên quan khác.</p>
+            <p>   – Chúng tôi nhận bảo hành dây đeo vĩnh viễn dành cho khách hàng nếu tình trạng dây lâu ngày bị giãn nở, cọ sát vớt đá gây đứt dây trong quá trình sử dụng, chi phí vận chuyển xin quý khách vui lòng tự thanh toán.</p>
+            </div>
+        </div>
+            </div>
+        </div>
+   
+    </div>
+    </div>
+    </div>
+
+    <?php
+        include("footer.php");
+    ?>
   
-    </div>
-
- 
-        
-        
-    </div>
-
-
+         
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
 </body>
 </html>
+
---
+++ Frontend/navbar.php
@@ -0,0 +1,52 @@
+<?php
+  echo"
+  <nav class='navbar navbar-expand-lg ' style='background-color:#B1B267'>
+  <button
+    class='navbar-toggler'
+    type='button'
+    data-toggle='collapse'
+    data-target='#navbarTogglerDemo03'
+    aria-controls='navbarTogglerDemo03'
+    aria-expanded='false'
+    aria-label='Toggle navigation'
+  >
+    <span class='navbar-toggler-icon'></span>
+  </button>
+  <a href='Home.html' >
+  <img src='pic/AN LẠC (1).png' width='120px'>
+</a>
+
+  <div class='collapse navbar-collapse' id='navbarTogglerDemo03'>
+    <ul class='navbar-nav mr-auto mt-lg-0'>
+      <li class='nav-item mx-2 '>
+         <a class='nav-link' href='home.php'><i class='fa fa-home'></i> Trang Chủ</a>
+      </li>
+      <li class='nav-item mx-2'>
+        <a class='nav-link' href='sanpham.php'><i class='fa fa-shopping-bag'></i> Sản phẩm</a>
+      </li>
+      <li class='nav-item mx-2'>
+      <a class='nav-link' href='#'><i class='fa fa-users'></i> Liên Hệ</a>
+    </li>
+    <li class='nav-item mx-2'>
+    <a class='nav-link' href='#'><i class='fa fa-atlas'></i> Bộ sưu tập</a>
+  </li>
+   
+    </ul>
+    <form class='form-inline my-2 my-lg-0'>
+      <input
+        class='form-control mr-sm-2'
+        type='search'
+        placeholder='Nhập Sản Phẩm'
+        aria-label='Search'
+      />
+      <button class='btn btn-success my-2 my-sm-0' type='submit'>
+        Tìm Kiếm 
+      </button>
+    </form>
+  </div>
+</nav>
+
+  
+  ";
+
+?>
